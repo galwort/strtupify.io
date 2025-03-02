c78d4da37824f6f3153ce24548045f2b949d3568
galwort <wthomasgorbett@gmail.com>
2025-03-01 21:59:57 -0500

    Generate register page and set to standalone

---
+++ ui/src/app/pages/register/register.page.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+  standalone: false,
+})
+export class RegisterPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}

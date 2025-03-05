2fa43bc33ee5e92b46d43ca924981488237e7aec
galwort <wthomasgorbett@gmail.com>
2025-03-04 21:28:23 -0500

    Update jobs function with new kv and error logging

--- api/jobs/__init__.py
+++ api/jobs/__init__.py
@@ -4,7 +4,7 @@ from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from openai import OpenAI
 
-vault_url = "https://kv-galwort.vault.azure.net/"
+vault_url = "https://kv-strtupifyio.vault.azure.net/"
 credential = DefaultAzureCredential()
 secret_client = SecretClient(vault_url=vault_url, credential=credential)
 client = OpenAI(api_key=secret_client.get_secret("OAIKey").value)
@@ -18,7 +18,10 @@ def gen_jobs(company_description):
         + "that the company should hire for. "
         + "You should generate no more than 8 job titles. "
         + "Reply in JSON format with the word 'jobs' as the key, "
-        + "and the job titles as a list of strings as the value."
+        + "and the job titles as a list of strings as the value. "
+        + "If there is an issue, leave the value as an empty list, "
+        + "and describe the issue in the 'error' key. "
+        + "If there is no issue, leave the 'error' key out."
     )
 
     messages = [{"role": "system", "content": system_message}]
@@ -26,20 +29,24 @@ def gen_jobs(company_description):
     messages.append(user_message)
 
     response = client.chat.completions.create(
-        model="gpt-4o",
+        model="gpt-4o-mini",
         response_format={"type": "json_object"},
         messages=messages,
     )
 
-    jobs = loads(response.choices[0].message.content)["jobs"]
-
-    return jobs
+    return response.choices[0].message.content
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     req_body = req.get_json()
     company_description = req_body["company_description"]
 
-    jobs = gen_jobs(company_description)
+    response = gen_jobs(company_description)
 
-    return func.HttpResponse(dumps({"jobs": jobs}), mimetype="application/json")
+    if "error" in jobs:
+        return func.HttpResponse(
+            dumps({"error": response["error"]}), mimetype="application/json"
+        )
+    else:
+        jobs = loads(response)["jobs"]
+        return func.HttpResponse(dumps({"jobs": jobs}), mimetype="application/json")

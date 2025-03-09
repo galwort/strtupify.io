a03c2401cee68b5e1b141421dc06d97de8a2a7be
galwort <wthomasgorbett@gmail.com>
2025-03-08 20:25:49 -0500

    Add register functinonality

--- ui/src/app/pages/register/register.page.ts
+++ ui/src/app/pages/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +9,25 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class RegisterPage implements OnInit {
-  constructor() {}
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit() {}
+
+  register() {
+    this.authService
+      .register(this.email, this.password)
+      .then(() => {
+        this.router.navigate(['/login']);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  navigateTo(page: string) {
+    this.router.navigateByUrl(`/${page}`);
+  }
 }

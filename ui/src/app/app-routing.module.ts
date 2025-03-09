428e578d0db345f703ec8710435718b79c1fd00f
galwort <wthomasgorbett@gmail.com>
2025-03-08 20:31:07 -0500

    Add auth guards

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,32 +1,47 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { AuthenticatedGuard } from './guards/authenticated.guard';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () =>
-      import('./pages/home/home.module').then((m) => m.HomePageModule),
-  },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
   {
+    path: 'home',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomePageModule),
+    canActivate: [AuthenticatedGuard],
+  },
+  {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
+    canActivate: [AuthGuard],
   },
   {
     path: 'register',
-    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+    loadChildren: () =>
+      import('./pages/register/register.module').then(
+        (m) => m.RegisterPageModule
+      ),
+    canActivate: [AuthGuard],
   },
   {
     path: 'application',
-    loadChildren: () => import('./pages/application/application.module').then( m => m.ApplicationPageModule)
+    loadChildren: () =>
+      import('./pages/application/application.module').then(
+        (m) => m.ApplicationPageModule
+      ),
+    canActivate: [AuthenticatedGuard],
   },
   {
     path: 'company',
-    loadChildren: () => import('./pages/company/company.module').then( m => m.CompanyPageModule)
+    loadChildren: () =>
+      import('./pages/company/company.module').then((m) => m.CompanyPageModule),
+    canActivate: [AuthenticatedGuard],
   },
 ];
 
---
+++ ui/src/app/guards/authenticated.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { map, take } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticatedGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> {
+    return this.authService.isAuthenticated().pipe(
+      take(1),
+      map((isAuthenticated) => {
+        if (isAuthenticated) {
+          return true;
+        }
+        return this.router.createUrlTree(['/login']);
+      })
+    );
+  }
+}

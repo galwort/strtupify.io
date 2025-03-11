291738eb796871f55763e1b0c93cf9179286b1f5
galwort <wthomasgorbett@gmail.com>
2025-03-10 21:14:08 -0400

    Pull roles into company page

--- ui/src/app/components/roles/roles.component.ts
+++ ui/src/app/components/roles/roles.component.ts
@@ -1,14 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { environment } from 'src/environments/environment';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+export const app = initializeApp(environment.firebase);
+export const db = getFirestore(app);
 
 @Component({
   selector: 'app-roles',
   templateUrl: './roles.component.html',
   styleUrls: ['./roles.component.scss'],
+  imports: [CommonModule, FormsModule],
 })
-export class RolesComponent  implements OnInit {
+export class RolesComponent implements OnInit {
+  roles: { id: string; title: string; count: number }[] = [];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
-  ngOnInit() {}
-
+  async ngOnInit() {
+    let segments = this.router.url.split('/');
+    let companyId = segments.length > 2 ? segments[2] : '';
+    if (!companyId) {
+      return;
+    }
+    let querySnapshot = await getDocs(
+      collection(db, 'companies/' + companyId + '/roles')
+    );
+    querySnapshot.forEach((docSnap) => {
+      this.roles.push({
+        id: docSnap.id,
+        title: docSnap.data()['title'],
+        count: 0,
+      });
+    });
+  }
 }

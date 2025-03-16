7f5e94acdee2ac7a5ae35b9e67597a873c2667d5
galwort <wthomasgorbett@gmail.com>
2025-03-15 21:22:20 -0400

    Bring in companies onto home page

--- ui/src/app/pages/home/home.page.ts
+++ ui/src/app/pages/home/home.page.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { environment } from 'src/environments/environment';
+
+const app = initializeApp(environment.firebase);
+const db = getFirestore(app);
 
 @Component({
   selector: 'app-home',
@@ -6,8 +12,16 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  companies: any[] = [];
 
   constructor() {}
 
+  async ngOnInit() {
+    const companiesSnapshot = await getDocs(collection(db, 'companies'));
+    this.companies = companiesSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+  }
 }
